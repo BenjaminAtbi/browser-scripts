cf7c149429c2f9da4a818e611cc8c7c6fd06492f
belithioben <benjaminatbi@gmail.com>
2018-08-29 00:35:29 -0700

    added first two scripts

---
+++ back hotkey.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         back hotkey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      *
+// @grant        none
+// ==/UserScript==
+
+document.addEventListener("keydown", function(event){
+    if(event.key == "Backspace" && event.ctrlKey){
+        window.history.back()
+    }
+})
---
+++ numbered google links.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         numbered google links
+// @namespace    www.google.ca/search
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      *
+// @grant        none
+// ==/UserScript==
+
+
+//extract hyperlink from result element
+function extractlink(elem){
+    console.log(elem)
+    if(elem.id == "imagebox_bigimages") {
+       return elem.getElementsByClassName("iu-card-header")[0];
+    } else {
+       return elem.getElementsByClassName("r")[0].getElementsByTagName("a")[0];
+    }
+}
+
+//extract result elements
+function extractElems(){
+    var results = document.getElementsByClassName("g");
+    var links = {length:0};
+    for(var i = 0; i < results.length; i++) {
+        //filters out bullshit with additional classes
+        if(results[i].className == "g") {
+            links.length++;
+            links[links.length] = extractlink(results[i]);
+        }
+    }
+    return links;
+}
+
+//insert label beside link
+function labelLink(link, num){
+    console.log("inserting", link);
+    link.insertAdjacentHTML("beforebegin", "<div class='numlabel' id='"+num+"' "+
+                            "style='position:absolute;"+
+                            "left: -1.5em;"+
+                            "border-style:solid;"+
+                            "border-width:2px;"+
+                            "border-color:#d8d8d8;"+
+                            "padding-left:.2em;"+
+                            "padding-right:.2em;"+
+                            "padding-top:.1em;"+
+                            "'>"+num+"</div>");
+}
+
+//set up document
+function lableResultLinks() {
+    links = extractElems();
+    console.log("links",links)
+    for(var i = 1; i <= links.length; i++){
+        labelLink(links[i], i);
+    }
+
+}
+
+//enter link determined by keypress
+function visitLinkByNum(event){
+    //if you press 0, visit link 10
+    if (event.key = "0" && links.length >= 10){
+        window.location.href = links[10].href;
+    //if you press n, visit link n
+    } else if (event.key <= links.length && event.key != 0){
+        window.location.href = links[event.key].href;
+    }
+
+}
+
+var links;
+document.addEventListener("keydown", visitLinkByNum);
+
+lableResultLinks();
+
+
+
