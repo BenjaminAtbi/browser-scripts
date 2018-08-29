71a216dfb2898bfd38953970fd1e5fe57035d057
belithioben <benjaminatbi@gmail.com>
2018-08-29 01:07:31 -0700

    google links -- added bug report. doesn't visit link if text box is highlighted

--- numbered google links.js
+++ numbered google links.js
@@ -8,10 +8,11 @@
 // @grant        none
 // ==/UserScript==
 
+//BUGS:
+// ** label beside "images for x" link is out of position. improve css
 
 //extract hyperlink from result element
 function extractlink(elem){
-    console.log(elem)
     if(elem.id == "imagebox_bigimages") {
        return elem.getElementsByClassName("iu-card-header")[0];
     } else {
@@ -35,7 +36,6 @@ function extractElems(){
 
 //insert label beside link
 function labelLink(link, num){
-    console.log("inserting", link);
     link.insertAdjacentHTML("beforebegin", "<div class='numlabel' id='"+num+"' "+
                             "style='position:absolute;"+
                             "left: -1.5em;"+
@@ -49,31 +49,31 @@ function labelLink(link, num){
 }
 
 //set up document
-function lableResultLinks() {
+function labelResultLinks() {
     links = extractElems();
-    console.log("links",links)
     for(var i = 1; i <= links.length; i++){
         labelLink(links[i], i);
     }
-
 }
 
 //enter link determined by keypress
 function visitLinkByNum(event){
     //if you press 0, visit link 10
-    if (event.key = "0" && links.length >= 10){
-        window.location.href = links[10].href;
-    //if you press n, visit link n
-    } else if (event.key <= links.length && event.key != 0){
-        window.location.href = links[event.key].href;
+    if (document.activeElement == document.getElementsByTagName("body")[0]){
+        if (event.key == "0" && links.length >= 10){
+            window.location.href = links[10].href;
+            //if you press n, visit link n
+        } else if (event.key <= links.length && event.key != 0){
+            window.location.href = links[event.key].href;
+        }
     }
-
 }
 
 var links;
 document.addEventListener("keydown", visitLinkByNum);
+labelResultLinks();
 
-lableResultLinks();
+console.log("focused", document.activeElement)
 
 
 
