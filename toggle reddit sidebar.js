c90d64c91b59ef3c4065bc0bc99986893ea2d23b
Benjamin Atbi <benjaminatbi@gmail.com>
2018-11-01 20:54:28 -0700

    comments resize to fill new space

--- toggle reddit sidebar.js
+++ toggle reddit sidebar.js
@@ -3,7 +3,7 @@
 // @namespace    http://www.reddit.com
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Benjamin Atbi
 // @include      *
 // @grant        none
 // ==/UserScript==
@@ -11,12 +11,16 @@
 document.addEventListener("keydown", function(event){
     if(event.key == "b" && event.ctrlKey){
         var sidebar = document.getElementsByClassName("side")[0]
+        var content = document.querySelectorAll('[role="main"]')[0]
+        console.log(content)
         if(sidebar.style.cssText == "display: none;"){
             console.log("none")
             sidebar.style.cssText = "display: block;"
+            content.style.cssText = "width: calc(100% - 325px);"
         }else{
             console.log(sidebar.style)
             sidebar.style.cssText = "display: none;"
+            content.style.cssText = "width: 100%;"
         }
     }
 })
