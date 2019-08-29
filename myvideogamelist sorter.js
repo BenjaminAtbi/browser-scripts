cc493683e52e6a9b89994c5c1fb89ba6bcd35a02
belithioben <benjaminatbi@gmail.com>
2019-08-28 21:15:55 -0700

    same

--- myvideogamelist sorter.js
+++ myvideogamelist sorter.js
@@ -1,23 +1,28 @@
 // ==UserScript==
 // @name         myvideogamelist sorter
-// @namespace    myvideogamelist.com/mylist/
 // @version      0.1
 // @description  sort list by rating
 // @author       You
-// @match        https://myvideogamelist.com/mylist/Belithioben
+// @match        https://myvideogamelist.com/list/
 // @grant        none
 // ==/UserScript==
 
-
 var sort_by_rating = function(a, b) {
-    return parseInt(b.childNodes[5].innerHTML) - parseInt(a.childNodes[5].innerHTML);
+    var anum = parseInt(b.childNodes[5].innerHTML);
+    var bnum = parseInt(a.childNodes[5].innerHTML);
+    if(isNaN(anum) && isNaN(bnum)){ return 0};
+    if(isNaN(anum)){ return -1};
+    if(isNaN(bnum)){ return 1};
+    return anum - bnum;
 }
 
 var tables = document.getElementsByTagName("tbody");
+console.log("tables: ",tables);
 
 var table;
 for (var i = 0; i < 3; i++){
     var items = Array.prototype.slice.call(tables[i].children);
+    console.log(items)
     items.sort(sort_by_rating);
 
     for (var y = 0; y < items.length; y++) {
