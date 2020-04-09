0d067092e96b1d327c9a16dd46f3736f1c77be16
BenjaminAtbi <benjaminatbi@gmail.com>
2020-04-08 17:02:30 -0700

    removed debug

--- toggle reddit sidebar.js
+++ toggle reddit sidebar.js
@@ -10,13 +10,10 @@ document.addEventListener("keydown", function(event){
     if(event.key == "b" && event.ctrlKey){
         var sidebar = document.getElementsByClassName("side")[0]
         var content = document.querySelectorAll('[role="main"]')[0]
-        console.log(content)
         if(sidebar.style.cssText == "display: none;"){
-            console.log("none")
             sidebar.style.cssText = "display: block;"
             content.style.cssText = "width: calc(100% - 325px);"
         }else{
-            console.log(sidebar.style)
             sidebar.style.cssText = "display: none;"
             content.style.cssText = "width: 100%;"
         }
